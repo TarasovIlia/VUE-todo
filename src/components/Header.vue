660b3ea47fa5a4c6bb36ced75f2eef9039e1bbde
Iliya Tarasav <Iliya.Tarasav@itechart-group.com>
2021-09-03 08:11:01 +0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+
+<script>
+export default {
+  data() {
+    return {
+      counter: false
+    }
+  },
+}
+</script>
+
+<template>
+      <header class="header-main-wrapper">
+        <div class="heder">
+          <h1>Cool header!</h1>
+          <button v-on:click="counter = !counter" class="button header-button">add new task</button>
+          <h1>{{ counter }}</h1>
+        </div>
+      </header>
+</template>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,37 @@
+<script>
+//import draggable from 'vuedraggable'
+export default {
+  data() {
+    return {
+      parts : null
+    }
+  },
+  created() {
+    fetch("http://localhost:3000/todoList")
+    .then(response => response.json(response))
+    .then(data => (this.parts = data))
+  },  
+}
+</script>
+
+<template>
+  <div class="todo-main-wrapper">
+    <section class="todo-list">
+      <div 
+        v-for="(part) in parts"
+        :key="part.title"
+        class="todo-item"
+      >
+        <h1>{{ part.id }}</h1>
+        <h1>{{ part.title }}</h1>
+        <p>{{ part.task }}</p>
+        <div :class="{
+          indicator: true,
+          non: part.done === true
+        }"
+        ></div>
+        <input type="checkbox" name="done" id="">
+      </div>
+    </section>
+  </div>
+</template>
